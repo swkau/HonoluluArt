95639d568b4ab4463c1fcd26c2f02e951bab3db0
Kau Si Wei <kausiwei@gmail.com>
2016-07-11 17:42:33 +0800

    geo location added

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
+{
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+        
+        // MARK: - Location Delegate Methods
+        func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            
+            let location = locations.last
+            
+            let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+            
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+            
+            self.mapView.setRegion(region, animated: true)
+            
+            self.locationManager.stopUpdatingLocation()
+            
+        }
+        
+        
     }
-
-
-
+        
 }
-
